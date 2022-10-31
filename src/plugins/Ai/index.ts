fa49601e0fb047fffc42194688537ea045dcb1b4
unknown <professionaltarget@gmail.com>
2022-10-31 18:16:00 -0400

    Added OpenAI integration for !ai command

--- src/plugins/Ai/index.ts
+++ src/plugins/Ai/index.ts
@@ -1,7 +1,7 @@
 import CommandPlugin from "../CommandPlugin"
-import fetch from "node-fetch"
+import openAi from "../../utils/OpenAiManager"
 
-export default class Cat extends CommandPlugin {
+export default class Ai extends CommandPlugin {
     public constructor() {
         super()
         this.register("!ai", this.ai.bind(this))
@@ -9,24 +9,8 @@ export default class Cat extends CommandPlugin {
 
     private async ai(from: string, args: string[], fullMessage: string): Promise<void> {
         try {
-            const trimmedMessage = fullMessage.trim()
-            const completeData = await fetch("https://api.eleuther.ai/completion", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    context: trimmedMessage,
-                    remove_input: true,
-                    response_length: 128,
-                    temp: 0.8,
-                    top_p: 0.9
-                })
-            })
-
-            const completeJson = await completeData.json()
-            const text = completeJson[0].generated_text
-            this.bot.sayToAll(trimmedMessage + text)
+            const text = await openAi.complete(fullMessage, Math.floor(128 + Math.random() * 128))
+            this.bot.sayToAll(text)
         }
         catch (e) {
             console.error(e)
---
+++ src/utils/OpenAiManager.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs"
+import fetch from "node-fetch"
+
+export class OpenAiManager {
+    private apiKey: string
+
+    public constructor(secretFile: string) {
+        const data = JSON.parse(fs.readFileSync(secretFile).toString())
+        this.apiKey = data.apiKey
+    }
+
+
+    public async complete(prompt: string, size: number = 128): Promise<string> {
+        const trimmedMessage = prompt.trim()
+        const completeData = await fetch("https://api.openai.com/v1/engines/text-davinci-002/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${this.apiKey}`
+            },
+            body: JSON.stringify({
+                prompt: trimmedMessage,
+                max_tokens: size,
+                temperature: 0.9,
+
+            })
+        })
+
+
+        const completeJson = await completeData.json()
+        return trimmedMessage + completeJson.choices[0].text
+    }
+}
+
+const manager = new OpenAiManager("./data/_Secrets/openai.json")
+
+export default manager
